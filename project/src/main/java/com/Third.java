40c4e367065a515ae5a7cb0e8d40ea45ba62ba69
laya068 <madesruthilaya19@gmail.com>
2024-11-19 06:35:02 +0530

    second and third java files

---
+++ project/src/main/java/com/Third.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Third {
+    public static void main(String[] args) { 
+        System.out.println("Hello from ThirdClass!"); 
+    }
+    
+}
---
+++ project/src/main/java/com/second.java
@@ -0,0 +1,8 @@
+package com;
+
+public class second {
+    public static void main(String[] args) { 
+        System.out.println("Hello from SecondClass!"); 
+    }
+    
+}
